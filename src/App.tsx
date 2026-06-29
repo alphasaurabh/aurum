7420cd68ab6b0b9d6517af825d175913aa876642
Rachna Bhandari <rachna.bhandari193@gmail.com>
2026-06-29 20:53:08 +0530

    fix: resolve merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import ProductShowcase from './components/ProductShowcase/ProductShowcase'
+import Carousel from './components/Carousel/Carousel'
 
 function App() {
   return (
     <div>
       <ProductShowcase />
-    </div>
-import Carousel from './components/Carousel/Carousel'
-
-function App() {
-  return (
-    <>
       <Carousel />
-    </>
+    </div>
   )
 }
 
